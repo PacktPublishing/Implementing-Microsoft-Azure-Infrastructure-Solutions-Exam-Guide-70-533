da3d30c2d4031e13e0c8cf5ea91ccfb38630868f
MelonyQ <urinaway@gmail.com>
2018-06-11 23:42:37 +0200

    add application web app connecting with database

---
+++ Chapter 4/infra70533webapp/infra70533webapp/Models/InfraNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infra70533webapp.Models
+{
+    public class InfraNote
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+
+        [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
