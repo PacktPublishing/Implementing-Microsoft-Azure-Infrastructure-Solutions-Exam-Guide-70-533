28403e933ae933946e61006c980b0c5a0b87e5e8
cloud_melon <urinaway@gmail.com>
2018-06-12 15:20:43 +0200

    Update script and refactor with variable
    Update script and refactor with variable replace resource group name

--- Chapter 4/CreateWebAppByPowerShell.ps1
+++ Chapter 4/CreateWebAppByPowerShell.ps1
@@ -3,16 +3,17 @@ Login-AzureRmAccount
 $subscription = "Visual Studio Entreprise"
 Set-AzureRmContext -Subscription $subscription
 
-$webappname="mywebapp$(Get-Random)"
+$webappname="infra70533pswebapp"
+$resgroupname = "infra70533pswebapp-rg"
 $location="West Europe"
 
 # Create a resource group.
-New-AzureRmResourceGroup -Name myResourceGroup -Location $location
+New-AzureRmResourceGroup -Name $resgroupname -Location $location
 
 # Create an App Service plan in Free tier.
 New-AzureRmAppServicePlan -Name $webappname -Location $location 
--ResourceGroupName myResourceGroup -Tier Free
+-ResourceGroupName $resgroupname -Tier Free
 
 # Create a web app.
 New-AzureRmWebApp -Name $webappname -Location $location 
--AppServicePlan $webappname -ResourceGroupName myResourceGroup
+-AppServicePlan $webappname -ResourceGroupName $resgroupname
