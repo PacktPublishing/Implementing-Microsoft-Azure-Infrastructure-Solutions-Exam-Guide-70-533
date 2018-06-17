881f6bbe9793e9b536f46c00826bb4c7d716e67e
MelonyQ <urinaway@gmail.com>
2018-06-17 22:48:22 +0200

    add resource group creation poewrshell

--- Chapter 4/02-Scripts/DeployWebAppARMByPowerShell.ps1
+++ Chapter 4/02-Scripts/DeployWebAppARMByPowerShell.ps1
@@ -5,6 +5,7 @@ $subscription = "Visual Studio Entreprise"
 Set-AzureRmContext -Subscription $subscription
 
 
-
+# Create a resource group.
+New-AzureRmResourceGroup -Name $resgroupname -Location $location
 
 New-AzureRmResourceGroupDeployment -Name "infrabasicapp" -ResourceGroupName "infra70533pswebapp-rg" -TemplateFile .\AzureDeploy.json -TemplateParameterFile  .\AzureDeploy.Parameters.json
