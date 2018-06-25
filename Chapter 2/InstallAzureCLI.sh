104b5fd44145d43cfb27597d85f18292fa21fe26
cloud_melon <urinaway@gmail.com>
2018-06-25 17:03:04 +0200

    Create InstallAzureCLI.sh
    Create InstallAzureCLI.sh file to install Azure CLI modules

---
+++ Chapter 2/InstallAzureCLI.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Take a look at https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest to get latest version of Azure CLI 
+
+brew update && brew install azure-cli
+
+az login 
+
+
