1902e6a52a43bd26bc33373b7bd16e8ec873920d
MelonyQ <urinaway@gmail.com>
2018-07-05 22:50:49 +0200

    refacto template and script in cp2

---
+++ Chapter 2/01 - Scripts /DeployWebAppARMByPowerShell.ps1
@@ -0,0 +1,15 @@
+Login-AzureRmAccount
+
+#At moment for one subscription 
+$subscription = "Visual Studio Entreprise"
+Set-AzureRmContext -Subscription $subscription
+
+
+# Create a resource group.
+$webappname="infra70533pswebapp"
+$resgroupname = "infra70533pswebapp-rg"
+$location="West Europe"
+
+New-AzureRmResourceGroup -Name $resgroupname -Location $location
+
+New-AzureRmResourceGroupDeployment -Name $webappname -ResourceGroupName $resgroupname -TemplateFile ./03-ARM/AzureDeploy.json -TemplateParameterFile  ./03-ARM/AzureDeploy.Parameters.json
